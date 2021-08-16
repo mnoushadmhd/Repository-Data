65ddf8586a9da2c8a533c3652782a77fd3023f65
mnoushadmhd <78750930+mnoushadmhd@users.noreply.github.com>
2021-08-15 19:47:34 -1200

    Add files via upload

---
+++ Zou Task-1/resources/js/script.js
@@ -0,0 +1,24 @@
+let btn1=document.getElementById("btn1");
+btn1.addEventListener("click",()=>{
+    alert("Returns Sign up Functionality ")
+})
+let btn2=document.getElementById("btn2");
+btn2.addEventListener("click",()=>{
+    alert("Returns Invest Now Functionality ")
+})
+let btn3=document.getElementById("btn3");
+btn3.addEventListener("click",()=>{
+    alert("Returns Browse Farm Functionality ")
+})
+let btn4=document.getElementById("btn4");
+btn4.addEventListener("click",()=>{
+    alert("Returns Learn More Functionality ")
+})
+let btn5=document.getElementById("btn5");
+btn5.addEventListener("click",()=>{
+    alert("Returns Inest Now Functionality ")
+})
+let btn6=document.getElementById("btn6");
+btn6.addEventListener("click",()=>{
+    alert("Returns Sign In Functionality ")
+})
